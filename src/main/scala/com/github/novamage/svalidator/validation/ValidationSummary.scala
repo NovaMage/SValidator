7eabf5fe321bc35fbc27375abeb87cfd645a5c79
Angel Blanco <angel.softworks@gmail.com>
2013-03-03 10:12:53 -0400

    Removing some code I added while brainstorming something else.

--- src/main/scala/com/github/novamage/svalidator/validation/ValidationSummary.scala
+++ src/main/scala/com/github/novamage/svalidator/validation/ValidationSummary.scala
@@ -5,8 +5,4 @@ import simple.SimpleValidator
 case class ValidationSummary(validationFailures: List[ValidationFailure]) {
 
   def isValid = validationFailures.isEmpty
-
-  class a extends SimpleValidator[ValidationResult] {
-    def buildRules = List(For(_.message).ForField("message"))
-  }
 }
