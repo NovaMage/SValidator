2d3abaf3e901610d2aae0c42492165491e856fb0
Angel Blanco <angel.softworks@gmail.com>
2014-03-02 17:55:05 -0400

    Refactored binding validator to reuse implementation of MappingBindingValidator.

--- src/main/scala/com/github/novamage/svalidator/validation/binding/BindingValidator.scala
+++ src/main/scala/com/github/novamage/svalidator/validation/binding/BindingValidator.scala
@@ -1,20 +1,11 @@
 package com.github.novamage.svalidator.validation.binding
 
-import com.github.novamage.svalidator.validation.simple.SimpleValidator
 import scala.reflect.runtime.{universe => ru}
-import com.github.novamage.svalidator.binding.binders.special.MapToObjectBinder
-import com.github.novamage.svalidator.validation.ValidationFailure
-import com.github.novamage.svalidator.binding.{BindingPass, BindingFailure}
 
-abstract class BindingValidator[A: ru.TypeTag] extends SimpleValidator[A] {
+abstract class BindingValidator[A: ru.TypeTag] extends MappingBindingValidator[A] {
 
   def bindAndValidate(valuesMap: Map[String, Seq[String]]): BindingAndValidationSummary[A] = {
-    val bindingResult = MapToObjectBinder.bind[A](valuesMap)
-    bindingResult match {
-      case BindingFailure(errors, cause) => BindingAndValidationSummary(errors.map(error => ValidationFailure(error.fieldName, error.errorMessage)), None, valuesMap)
-      case BindingPass(value) => BindingAndValidationSummary(validate(value).validationFailures, Some(value), valuesMap)
-    }
-
+    super.bindAndValidate(valuesMap, (x: A) => x)
   }
 
 }
