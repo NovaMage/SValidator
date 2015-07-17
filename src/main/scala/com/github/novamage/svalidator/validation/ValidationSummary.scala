5c14a5eed119ec321f1cb2fc4468ca3406faabb9
NovaMage <angel.softworks@gmail.com>
2015-07-17 08:12:05 -0400

    Added ability of merging validation summaries and summary metadata. Increased version number to 0.6.1.

--- src/main/scala/com/github/novamage/svalidator/validation/ValidationSummary.scala
+++ src/main/scala/com/github/novamage/svalidator/validation/ValidationSummary.scala
@@ -1,14 +1,31 @@
 package com.github.novamage.svalidator.validation
 
 
-class ValidationSummary(val validationFailures: List[ValidationFailure]) {
+class ValidationSummary(val validationFailures: List[ValidationFailure],
+                        val metadata: Map[String, Any] = Map.empty[String, Any]) {
 
   def isValid = validationFailures.isEmpty
+
+  def merge(another: ValidationSummary): ValidationSummary = {
+    val otherMetadata = another.metadata
+    val allKeys = metadata.keySet ++ otherMetadata.keySet
+    val mergedMetadata = allKeys.map { key =>
+      if (metadata.contains(key) && otherMetadata.contains(key)) {
+        key -> List(metadata(key), otherMetadata(key))
+      } else if (metadata.contains(key)) {
+        key -> metadata(key)
+      } else {
+        key -> otherMetadata(key)
+      }
+    }
+    new ValidationSummary(validationFailures ++ another.validationFailures, mergedMetadata.toMap)
+  }
+
 }
 
 object ValidationSummary {
 
-  def apply(validationFailures: List[ValidationFailure]): ValidationSummary = new ValidationSummary(validationFailures)
+  def apply(validationFailures: List[ValidationFailure], metadata: (String, Any)*) = new ValidationSummary(validationFailures, metadata.toMap)
 
-  final val Empty = new ValidationSummary(Nil)
+  final val Empty: ValidationSummary = new ValidationSummary(Nil)
 }
